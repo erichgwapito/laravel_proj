dda811cbde05ba774c186db73ee56decd9eb7a03
Nash Erich S. Garcia <163651023+erichgwapito@users.noreply.github.com>
2026-07-03 10:28:44 +0800

    feat: implement interactive click counter using alpine js and fix nginx 504 error

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,11 @@ Route::livewire('/counter', 'pages::counter');
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
